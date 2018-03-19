530aedebf8456e78497110bf26cbfecc25c6c430
Rusty Pedrosa <rusty@pedrosatech.com>
2018-03-19 01:55:11 -0700

    Add viewMenu, navMenu

--- src/MyCalendarTester.java
+++ src/MyCalendarTester.java
@@ -1,9 +1,6 @@
 import com.sun.tools.javac.Main;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.util.*;
 
 import java.text.SimpleDateFormat;
 import java.text.DateFormatSymbols;
@@ -12,8 +9,9 @@ public class MyCalendarTester
 {
     public static void main(String [] args)
     {
-        // Create menus
-        Menu mainMenu = new Menu("Main Menu", "Select one of the following options:");
+        // --------- Create menus ---------
+        // Main menu
+        Menu mainMenu = new Menu("", "Select one of the following options:");
         mainMenu.addChoice("[L]oad",        null);
         mainMenu.addChoice("[V]iew by",     null);
         mainMenu.addChoice("[C]reate",      null);
@@ -22,56 +20,80 @@ public class MyCalendarTester
         mainMenu.addChoice("[D]elete",      null);
         mainMenu.addChoice("[Q]uit",        null);
 
+        // View menu
+        Menu viewMenu = new Menu("", "Select a view:");
+        viewMenu.addChoice("[D]ay view",  null);
+        viewMenu.addChoice("[M]onth view",null);
 
+        // Nav menu
+        Menu navMenu = new Menu("", "");
+        navMenu.addChoice("[P]revious",    null);
+        navMenu.addChoice("[N]ext",        null);
+        navMenu.addChoice("[M]ain menu",   null);
 
-        // In case execution spans change of day, this calendar shall represent "today" for the rest of execution
+        // --------- Create calendar ---------
         MyCalendar cal = new MyCalendar(); // defaults to today's date
-        cal.set(Calendar.DAY_OF_MONTH, 31);
+
+        // --------- Required screenshots ---------
+        //Screen 1: Show the initial calendar after starting the application
         cal.printMonth();
         System.out.println();
 
         //Shows prompt and gets user input (first character)
-        char choice = mainMenu.prompt();
-        mainMenu.executeOption(choice);
+        while (true) {
+            switch (mainMenu.prompt()) {
+                case 'l':   //load
+                    // The system loads events.txt to populate the calendar.
+                    break;
+                case 'v':   //view by
+                    char viewChoice = viewMenu.prompt();
 
+                    boolean exitView = false;
+                    while (!exitView) {
+                        switch (viewChoice) {
+                            case 'd':
+                                //Display day
+                                break;
 
+                            case 'm':
+                                //Display month
+                                cal.printMonth();
+                                break;
+                        }
 
-
-//        MainMenuChoice choice = mainMenu.prompt();
-//
-//        mainloop:
-//        switch (choice)
-//        {
-//            case LOAD: // The system loads events.txt to populate the calendar.
-//                break;
-//            case VIEWBY: //
-//                switch (viewMenu.getChoice())
-//                {
-//                    case DAY:
-//                        break;
-//                    case MONTH:
-//                        break;
-//                }
-//                break;
-//            case CREATE: // This option allows the user to schedule an event.
-//                break;
-//            case GOTO: //
-//                break;
-//            case EVENTLIST: //
-//                break;
-//            case DELETE: //
-//                break;
-//            case QUIT: //
-//                break;
-//
-//        }
+                        switch (navMenu.prompt()) {
+                            case 'p':   //Prev
+                                cal.add(viewChoice == 'd' ? Calendar.DAY_OF_MONTH : Calendar.MONTH, -1);
+                                break;
+                            case 'n':   //Next
+                                cal.add(viewChoice == 'd' ? Calendar.DAY_OF_MONTH : Calendar.MONTH, 1);
+                                break;
+                            case 'm':   //Main menu
+                                //todo: reset cal to today's date?
+                                exitView = true;
+                                break;
+                        }
+                    }
+                    break;  //end View by
+                //}
+                case 'c':   //create
+                    // This option allows the user to schedule an event.
+                    break;
+                case 'g':   //go to
+                    break;
+                case 'e':   //event list
+                    break;
+                case 'd':   //delete
+                    break;
+                case 'q':   //quit
+                    break;
+            }
+        }
 //        Scanner sc = new Scanner(System.in);
 //        printCalendar(cal);
 ////        "Select one of the following options: \n"
 ////        "[L]oad  [V]iew by  [C]reate  [G]o to  [E]vent list  [D]elete  [Q]uit"
 //
-//        //Required screenshots:
-//        //Screen 1: Show the initial calendar after starting the application
 //
 //        //Screen 2: [L]oad your events.txt and show [E]vent list
 //
